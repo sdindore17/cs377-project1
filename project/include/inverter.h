525b67592ed8d8f5eef2e30b93ad7d308f077c34
sdindore17 <90989361+sdindore17@users.noreply.github.com>
2023-02-20 14:57:04 -0500

    Add files via upload

---
+++ project/include/inverter.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string build_inverted_index(string filename);
---
+++ project/src/inverter.cpp
@@ -0,0 +1,13 @@
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+
+#include <inverter.h>
+
+using namespace std;
+
+
+string build_inverted_index(string filename) {
+  return "";
+}
